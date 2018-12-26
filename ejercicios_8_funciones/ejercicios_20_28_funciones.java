7b6c88352d5ef9897492f5c16854ff88f12b3f16
Mercedes perea <m.mercedes.perea@gmail.com>
2018-12-26 16:49:45 +0100

    Ejercicios funciones

---
+++ ejercicios_8_funciones/ejercicios_20_28_funciones.java
@@ -0,0 +1,91 @@
+/*
+ * Ejercicios del 20 al 28
+ */
+
+
+public class ejercicios_20_28_funciones {
+	
+	public static void main (String[] args) {
+    
+    //rellenar un array 
+    System.out.print("Vamos a generar un array de numeros aleatorios\n");
+    System.out.println();
+    System.out.print("Deme la longitud del array\n");
+    int longitud=Integer.parseInt(System.console().readLine());
+    
+    System.out.print("Ahora vamos a decir el parametro del numero maximo que comprondra el array\n");
+    int maximo=Integer.parseInt(System.console().readLine());
+    
+    System.out.print("Ahora deme el numero minimo que compondra el array\n");
+    int minimo=Integer.parseInt(System.console().readLine());
+    
+    int[] a = (funciones.funciones_array.generaArrayInt(longitud,maximo,minimo));     // este array se generara con los valores indicados y segun lo especificado en la funcion destinada a ello
+    
+    System.out.print("Su array generado aleatoriamente entre los valores que ha decidido es: \n");
+    //muestra el array
+    for(int i=0;i<a.length;i++){
+        System.out.print(a[i]+ " ");
+      }
+    System.out.println();
+    System.out.println();
+    
+    //saca el maximo de un array
+    int max=(funciones.funciones_array.maximoArrayInt(a));
+    System.out.print("El maximo del array es: "+max+"\n");
+    System.out.println();
+    //saca el minimo de un array
+    int min=(funciones.funciones_array.minimoArrayInt(a));
+    System.out.print("El minimo del array es : "+min+"\n");
+    System.out.println();
+    //saca la media  de un array
+    int media=(funciones.funciones_array.mediaArrayInt(a));
+    System.out.print("La media del array es : "+media+"\n");
+    
+    System.out.println();
+    //dice si un numero esta o no en el array
+    System.out.print("Introduzca un numero y le dire si esta en el array o no\n");
+    int numero = Integer.parseInt(System.console().readLine());
+    if (funciones.funciones_array.estaEnArrayInt(a,numero)) {
+      System.out.println("El " + numero + " esta en el array .");
+      } else {
+          System.out.println("El " + numero + " no esta en el array.");
+            }
+    System.out.println();
+    
+    //saca la posicion de un numero en el array
+       System.out.print("Introduzca el numero del que quiere saber la posicion dentro del array \n");
+    int numeposi = Integer.parseInt(System.console().readLine());
+    int posicion=(funciones.funciones_array.posicionEnArrayInt(a,numeposi));
+    System.out.print("La posicion es : "+posicion+"\n");
+		
+    System.out.println();
+    //voltea un array 
+    int[] volteado = (funciones.funciones_array.volteaArrayInt(a));     // este array se generara con los valores indicados y segun lo especificado en la funcion destinada a ello
+    System.out.print("El array volteado es: \n");
+    for(int i=0;i<a.length;i++){
+      System.out.print(volteado[i]+ " ");
+    }
+    System.out.println();
+   //Rota n posiciones a la derecha los números de un array.
+    System.out.print("\nIntroduzca el numero de posiciones a la derecha que quiere mover su array\n");
+    int n = Integer.parseInt(System.console().readLine());
+    int[] rotado= (funciones.funciones_array.rotaDerechaArrayInt(a,n));   
+    System.out.print("El array movido es: \n");
+    for(int i=0;i<a.length;i++){
+      System.out.print(rotado[i]+ " ");
+    }
+    System.out.println();
+    //Rota n posiciones a la izquierda los números de un array.
+    System.out.print("\nIntroduzca el numero de posiciones a la izquierda que quiere mover su array\n");
+    int ni = Integer.parseInt(System.console().readLine());
+    int[] rotado2= (funciones.funciones_array.rotaIzquierdaArrayInt(a,ni));   
+    System.out.print("El array movido es: \n");
+    for(int i=0;i<a.length;i++){
+      System.out.print(rotado2[i]+ " ");
+    }
+    
+    
+    
+	}
+}
+
--- ejercicios_8_funciones/funciones/funciones_array.java
+++ ejercicios_8_funciones/funciones/funciones_array.java
@@ -15,7 +15,124 @@ public class funciones_array {
       }
       return x;
     }
+    
+    
+      //2. minimoArrayInt: Devuelve el minimo del array que se pasa como parámetro.
+  public static int minimoArrayInt(int[] x) {     // para pasar un array como parametro
+    int maximo =0;
+    
+    for (int i=0; i<x.length;i++){
+      if (x[i]>maximo){
+        maximo=x[i];
+        }
+      }
+      int minimo=maximo;
+    for (int i=0; i<x.length;i++){
+      if (x[i]<minimo){
+        minimo=x[i];
+        }
+      }
+    return minimo;
+    }
+    
+    //3. maximoArrayInt: Devuelve el máximo del array que se pasa como parámetro.
+  public static int maximoArrayInt(int[] x) {     // para pasar un array como parametro
+    int maximo =0;
+    for (int i=0; i<x.length;i++){
+      if (x[i]>maximo){
+        maximo=x[i];
+        }
+      }
+    return maximo;
+    }
+    
+   //4. mediaArrayInt: Devuelve la media del array que se pasa como parámetro.
+   public static int mediaArrayInt(int[] x) { 
+     int media=0;
+     int suma=0;
+     for (int i=0; i<x.length;i++){
+      suma=suma+x[i];
+      }
+      media=suma/x.length;
+    return media;
+   }
+   
+   //5. estaEnArrayInt: Dice si un número está o no dentro de un array.
+   public static boolean estaEnArrayInt(int[] x, int n) {
+     boolean esta=false;
+    for (int i = 0; i < x.length; i++) {
+      if (x[i] == n) {
+        esta= true;
+      }
+    }
+    return esta;
   }
+  
+  //6. posicionEnArray: Busca un número en un array y devuelve la posición (el índice) en la que se encuentra.
+   public static int posicionEnArrayInt(int[] x, int n) {
+     int posicion=0;
+    for (int i = 0; i < x.length; i++) {
+      if (x[i] == n) {
+        posicion=i+1;
+      }
+    }
+    return posicion;
+  }
+  
+  //7. volteaArrayInt: Le da la vuelta a un array.
+  
+  public static int[] volteaArrayInt(int[] x) {
+     int[] volteado = new int[x.length]; 
+      
+      for (int i=0;i<x.length;i++){
+        volteado[x.length-i-1]=x[i];
+        }
+        return volteado;
+  }
+  
+  //8. rotaDerechaArrayInt: Rota n posiciones a la derecha los números de un array.
+  
+      public static int[] rotaDerechaArrayInt(int[] x, int n) {
+        
+        int[] a = x.clone(); // clona en a el contenido de x
+        int aux;
+        while (n > 0) {     //mientras que el n sea mayor que 0
+          n--;              // ve restando valores a n
+          aux = a[a.length - 1];   // el auxiliar tendra el valor maximo del array se usa el menos 1 por que realmente el array de 20 tiene posicion hasta el 19 ( del 0 al 19) osea 20 posiciones
+          for(int i = a.length - 1; i > 0; i--) {  // i tiene el valor maximo del array y se ira descontando de arriba a abajo
+              a[i] = a[i - 1];                   // el array a ( en la maxima posicion ira cogiendo valores del array a menos 1 posicion ( ejemplo en la posicion 19 se pondra el valor de la posicion 20 , en el 18 el del 19) y asi cada ronda se iran cambiando posiciones hasta que el while n llegue a 0
+            }
+          a[0] = aux;                           // el array a en la posicion 0 ira pillando en cada ronda el valor que va quedando sin sitio  como seria en el ejemplo anterior el valor que venga de la posicion 20 
+        }
+          return a;
+        }
+        
+        
+  //9. rotaIzquierdaArrayInt: Rota n posiciones a la izquierda los números de un array.
+  
+      public static int [] rotaIzquierdaArrayInt(int[] x, int n){
+        int [] a = x.clone();
+        int aux;
+        while(n>0){
+          n--;
+          aux= a[0];
+          for (int i=0; i<a.length-1;i++){
+            a[i]=a[i+1];
+            }
+          a[a.length-1]=aux;
+          }
+          return a;
+        
+        
+        
+        }
+  
+        
+    
+    
+}
+  
+  
     
  
     
